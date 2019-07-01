55bc2cac1827c7ef3912bc3bdb992be037440bf6
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-07-01 10:42:41 -0400

    updated alerts.js removal of if in _alerts.html.erb

--- app/assets/javascripts/plugins/alerts.js
+++ app/assets/javascripts/plugins/alerts.js
@@ -1,16 +1,5 @@
-// Jquery Version Sort of works removes alert on any click
-// $(document).on('click', '.exit-alert', function () {
-//   $(this).parent().hide();
-// }); 
-
-// Javascript version using EventListener
 document.addEventListener('click', function (event) {
-  // if we have the correct click event proceed
   if (event.target.parentNode.matches('.exit-alert')) {
-    // set the correct div we need to remove
-    element = event.target.parentNode.parentNode;
-    // remove div
-    element.parentNode.removeChild(element);
+    event.target.parentNode.parentNode.remove()
   }
-}, false);
-
+});
